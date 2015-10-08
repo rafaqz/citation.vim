19c6c7d44deb73225a3167df42dcd3028f01aeb8
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-10-08 18:18:59 +0900

    Change to staticmethod

--- src/unite_bibtex.py
+++ src/unite_bibtex.py
@@ -2,7 +2,7 @@
 
 import os.path
 from pybtex.database.input import bibtex
-from pbtex import errors
+from pybtex import errors
 
 
 class unite_bibtex(object):
@@ -11,18 +11,20 @@ class unite_bibtex(object):
     (not to pollute global name space)
     """
 
-    def _read_file(self, filename):
-        errors.enable_strict_mode()
+    @staticmethod
+    def _read_file(filename):
         parser = bibtex.Parser()
         return parser.parse_file(filename)
 
-    def _check_path(self, path):
+    @staticmethod
+    def _check_path(path):
         path = os.path.abspath(os.path.expanduser(path))
         if not os.path.exists(path):
             raise RuntimeError("file:%s not found" % path)
         return path
 
-    def entry_to_str(self, entry):
+    @staticmethod
+    def entry_to_str(entry):
         try:
             persons = entry.persons[u'author']
             authors = [unicode(au) for au in persons]
@@ -34,16 +36,29 @@ class unite_bibtex(object):
         desc = u"%s %s %s(%s)" % (",".join(authors), title, journal, year)
         return desc.replace("'", "").replace("\\", "")
 
-    def get_entries(self, bibpath_list):
+    @staticmethod
+    def get_entries(bibpath_list):
         entries = {}
         for bibpath in bibpath_list:
-            path = self._check_path(bibpath)
-            bibdata = self._read_file(path)
+            try:
+                path = unite_bibtex._check_path(bibpath)
+                bibdata = unite_bibtex._read_file(path)
+            except Exception as e:
+                print("Fail to read {}".format(bibpath))
+                print("Message: {}".format(str(e)))
+                continue
             for key in bibdata.entries:
                 try:
                     k = key.encode("utf-8")
                 except:
                     print("Cannot encode bibtex key, skip: {}".format(k))
                     continue
-                entries[k] = self.entry_to_str(bibdata.entries[key]).encode("utf-8")
+                entries[k] = unite_bibtex.entry_to_str(bibdata.entries[key]).encode("utf-8")
         return entries
+
+if __name__ == '__main__':
+    import sys
+    bibpath_list = sys.argv[1:]
+    entries = unite_bibtex.get_entries(bibpath_list)
+    for k, v in entries.items():
+        print("{}:{}".format(k, v))
