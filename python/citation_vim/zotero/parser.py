b3c06e5a564e0adf424d7597a0fc2e7e2ac06697
Rafael Schouten <rafaelschouten@gmail.com>
2017-06-16 13:08:57 +1000

    cast of all fields as unicode

--- python/citation_vim/zotero/parser.py
+++ python/citation_vim/zotero/parser.py
@@ -4,10 +4,12 @@ import os
 import json
 import shutil
 import sqlite3
+import collections
+import string
 import re
 from citation_vim.zotero.data import ZoteroData
 from citation_vim.zotero.betterbibtex import BetterBibtex
-from citation_vim.utils import check_path, raiseError
+from citation_vim.utils import check_path, raiseError, compat_str
 from citation_vim.item import Item
 
 class ZoteroParser(object):
@@ -20,7 +22,6 @@ class ZoteroParser(object):
         self.key_format = context.key_format
         self.clean_regex = re.compile("[^A-Za-z0-9\ \!\$\&\*\+\-\.\/\:\;\<\>\?\[\]\^\_\`\|]+")
         self.html_regex = re.compile('<[^<]+?>')
-
 
     def load(self):
         """
@@ -67,7 +68,9 @@ class ZoteroParser(object):
         return items
 
     def clean(self, string):
-        string = self.html_regex.sub('', string)
+        string = compat_str(string) # Cast as a unicode string in python 2 or 3
+        string = self.html_regex.sub('', string) # Remove any html formatting 
+        # Clean and return (based on field cleaning replacements in Zoteros BibLatex.js)
         return self.clean_regex.sub('', string)
         
 
@@ -77,17 +80,17 @@ class ZoteroParser(object):
         A user formatted key if present, or a better bibtex key, or zotero hash.
         """
         if self.context.key_format > "":
-            title = zot_item.title.lower()
+            title = compat_str(zot_item.title.lower())
             title = self.context.key_title_banned_regex.sub("", title)
             title = title.partition(" ")[0]
             date = item.date # Use the allready formatted date
-            author = zot_item.format_first_author().replace(" ", "_")
+            author = compat_str(zot_item.format_first_author().replace(" ", "_"))
             replacements = {
                 u"title": title.lower(),
                 u"Title": title.capitalize(), 
                 u"author": author.lower(), 
                 u"Author": author.capitalize(),
-                u"date": date.replace(' ', '-').capitalize() # Date may be 'In-press' 
+                u"date": date.replace(' ', '-').capitalize() # Date may be 'In-press' etc.
             }
             key_format = u'%s' % self.context.key_format
             key = key_format.format(**replacements)
@@ -97,4 +100,3 @@ class ZoteroParser(object):
             return citekeys[zot_item.id]
         else:
             return zot_item.key
-
