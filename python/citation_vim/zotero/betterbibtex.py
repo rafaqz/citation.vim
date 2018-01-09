8b7585de68fca6d0c216c9dee6a4f068abca40e4
Rafael Schouten <rafaelschouten@gmail.com>
2018-01-09 20:59:40 +1100

    update better bibtex loading for zotero 5

--- python/citation_vim/zotero/betterbibtex.py
+++ python/citation_vim/zotero/betterbibtex.py
@@ -9,14 +9,13 @@ import sqlite3
 class BetterBibtex(object):
 
     def __init__(self, zotero_path, cache_path):
-        self.bb_file = os.path.join(zotero_path, 'better-bibtex/db.json')
-        self.bb_database = os.path.join(zotero_path, 'betterbibtex-lokijs.sqlite')
-        self.bb_copy = os.path.join(cache_path, 'betterbibtex.sqlite')
+        self.bb_database = os.path.join(zotero_path, 'better-bibtex.sqlite')
+        self.bb_copy = os.path.join(cache_path, 'better-bibtex.sqlite')
 
     bb_data_query = u"""
-        select lokijs.data
-        from lokijs
-        where lokijs.name = "db.json"
+        select data
+        from "better-bibtex"
+        where name = "better-bibtex.citekey"
         """
 
     def load_citekeys(self):
@@ -24,33 +23,22 @@ class BetterBibtex(object):
         Loads better-bibtex citekeys if they exist.
         """
 
-        # The storage method for betterbibtex keeps changing so we'll try a few.
+        # The storage method for betterbibtex citations is currently json in a sqlite database cell.
         try:
-            bb_data = open(self.bb_file).read()
+            shutil.copyfile(self.bb_database, self.bb_copy)
+            conn = sqlite3.connect(self.bb_copy)
+            cur = conn.cursor()
+            cur.execute(self.bb_data_query)
+            bb_data = cur.fetchone()[0]
             bb_json = json.loads(bb_data)
         except:
-            try:
-                desc_strings.append(getattr(entry, desc_field))
-                shutil.copyfile(self.bb_database, self.bb_copy)
-                conn = sqlite3.connect(self.bb_copy)
-                cur = conn.cursor()
-                cur.execute(self.bb_data_query)
-                bb_data = cur.fetchone()[0]
-                bb_json = json.loads(bb_data)
-            except:
-                return {}
-
-        if not 'collections' in bb_json:
             return {}
 
-        citekeys = {}
-        for collection in bb_json['collections']:
-            # The array has variable structure so check all collections for key data
-            if all (k in collection for k in ("name","data")) and collection['name'] == 'keys':
-                for item in collection['data']:
-                    if all (k in item for k in ("itemID","citekey")):
-                        citekeys[item['itemID']] = item['citekey']
-                    else:
-                        citekeys[item['itemID']] = ""
-
-        return citekeys
+        bb_citekeys = {}
+        for item in bb_json['data']:
+            # Check item contains required fields 
+            if all (key in item for key in ("itemID", "citekey")):
+                bb_citekeys[item['itemID']] = item['citekey']
+            else:
+                bb_citekeys[item['itemID']] = ""
+        return bb_citekeys
--- python/citation_vim/zotero/parser.py
+++ python/citation_vim/zotero/parser.py
@@ -33,10 +33,10 @@ class ZoteroParser(object):
         zotero = ZoteroData(self.context)
         zot_data = zotero.load()
         bb = BetterBibtex(self.zotero_path, self.cache_path)
-        citekeys = bb.load_citekeys()
-        return self.build_items(zot_data, citekeys)
+        bb_citekeys = bb.load_citekeys()
+        return self.build_items(zot_data, bb_citekeys)
 
-    def build_items(self, zot_data, citekeys):
+    def build_items(self, zot_data, bb_citekeys):
         items = []
         for zot_id, zot_item in zot_data:
             item = Item()
@@ -59,7 +59,7 @@ class ZoteroParser(object):
             item.notes       = self.clean(zot_item.format_notes())
             item.tags        = self.clean(zot_item.format_tags())
             item.zotero_key  = self.clean(zot_item.key)
-            item.key         = self.format_key(item, zot_item, citekeys)
+            item.key         = self.format_key(item, zot_item, bb_citekeys)
             item.combine()
             items.append(item)
         return items
@@ -71,7 +71,7 @@ class ZoteroParser(object):
         return self.clean_regex.sub('', string)
 
 
-    def format_key(self, item, zot_item, citekeys):
+    def format_key(self, item, zot_item, bb_citekeys):
         """
         Returns:
         A user formatted key if present, or a better bibtex key, or zotero hash.
@@ -93,7 +93,7 @@ class ZoteroParser(object):
             key = key_format.format(**replacements)
             key = self.context.key_clean_regex.sub("", key)
             return key
-        elif zot_item.id in citekeys:
-            return citekeys[zot_item.id]
+        elif zot_item.id in bb_citekeys:
+            return bb_citekeys[zot_item.id]
         else:
             return zot_item.key
